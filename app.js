3369e1019e2c4c270ea228ca4ada82ec81299e51
ramadoni16 <donicyberit@gmail.com>
2025-02-10 22:33:32 +0700

    If Statment

---
+++ app.js
@@ -0,0 +1,16 @@
+// console.log("sebelum pembanding");
+// if (1 + 1 === 2) {
+//   console.log("di dalam pembanding");
+// }
+
+// console.log("setelah pembanding");
+
+let angka = Math.random();
+console.log(angka);
+
+if (angka >= 0.5) {
+  console.log("Angka lebih besar dari 0.5");
+}
+if (angka <= 0.5) {
+  console.log("Angka lebih kecil dari 0.5");
+}
