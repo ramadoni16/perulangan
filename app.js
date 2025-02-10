6c360853fd4ca15aa73d4c2ca21f1b4cd7d98a3d
ramadoni16 <donicyberit@gmail.com>
2025-02-10 23:03:09 +0700

    else

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-let age = 15;
+const iniHari = prompt("isikan hari!").toLowerCase();
 
-// if (age === 18) {
-//   console.log("boleh... boleh masuk");
-// } else if (age === 26) {
-//   console.log("perlu di bimbing orang tua ya");
-// } else if (age === 8) {
-//   console.log("Sudah pergi kedokter gigi ?");
-// }
-
-// const iniHari = "sabtu";
-// if (iniHari === "Senin") {
-//   console.log("selamat bekerja kawan");
-// } else if (iniHari === "sabtu") {
-//   console.log("semoga jangan cepat berlalu");
-// }
-
-const nilai = "76";
-
-if (nilai < 50) {
-  console.log("E");
-} else if (nilai < 60) {
-  console.log("D");
-} else if (nilai < 70) {
-  console.log("C");
-} else if (nilai < 80) {
-  console.log("B");
-} else if (nilai < 100) {
-  console.log("A");
+if (iniHari === "senin") {
+  console.log("selamat bekerja kawan");
+} else if (iniHari === "sabtu") {
+  console.log("semoga jangan cepat berlalu");
+} else {
+  console.log("Yah biasa saja");
 }
