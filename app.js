f15e5e7449cdfbe184dca57c923b8c08424dd6f8
ramadoni16 <donicyberit@gmail.com>
2025-02-11 09:34:04 +0700

    Struktur Data Array

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const balonku = prompt("input nama balon");
-switch (balonku) {
-  case "merah":
-    console.log("warna merah");
-    break;
-  case "hijau":
-    console.log("dooor");
-    break;
-  default:
-    console.log("bukan balon saya");
-    break;
-}
+let playlist = ["in the end", "celengan rindu", "ini ibu budi"];
+console.log(playlist[0]);
