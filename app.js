eeeaa6769e5625dde4b3ab66c656fd7b2a40856d
ramadoni16 <donicyberit@gmail.com>
2025-02-13 13:41:35 +0700

    Cara Memanggil Data Dari Object 2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const orang = {
-  nama: "rama",
-  tanggal: 16,
-  alamat: "lampung",
+// const orang = {
+//   nama: "rama",
+//   tanggal: 16,
+//   alamat: "lampung",
+// };
+
+// orang.nama;
+// orang.tanggal;
+// orang.alamat;
+
+// console.log(orang);
+
+barang = {
+  judul: "sepatu",
+  harga: 200000,
+  isReady: true,
+  size: [38, 39, 40, 41, 42],
+  location: {
+    indonesia: 48,
+    singapore: 50,
+  },
 };
-
-orang.nama;
-orang.tanggal;
-orang.alamat;
-
-console.log(orang);
+console.log(barang.location.indonesia);
