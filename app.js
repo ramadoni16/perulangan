0f378bba024bb877dbf7b20a9cae01188fc51790
ramadoni16 <donicyberit@gmail.com>
2025-02-14 14:43:52 +0700

    Cara Membuat Object Berjalan

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-// const orang = {
-//   nama: "rama",
-//   tanggal: 16,
-//   alamat: "lampung",
-// };
-
-// orang.nama;
-// orang.tanggal;
-// orang.alamat;
-
-// console.log(orang);
-
-barang = {
-  judul: "sepatu",
-  harga: 200000,
-  isReady: true,
-  size: [38, 39, 40, 41, 42],
-  location: {
-    indonesia: 48,
-    singapore: 50,
-  },
+const orang = {
+  nama: "rama",
+  tanggal: 16,
+  alamat: "lampung",
 };
-console.log(barang.location.indonesia);
+
+orang["nama"] = "doni";
+
+console.log(orang.nama);
