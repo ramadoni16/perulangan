dda3730e2ff3c12e1f6f6992a3c69e28e663c81f
ramadoni16 <donicyberit@gmail.com>
2025-02-15 00:24:40 +0700

    ex perulangan for

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 3) {
+//   console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i -= 2) {
+//   console.log(i);
+// }
+
+for (let i = 10; i <= 100000; i *= 20) {
   console.log(i);
 }
