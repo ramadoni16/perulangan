f0c6b11a2bb35db4c91e453fd75ed578c46db4be
ramadoni16 <donicyberit@gmail.com>
2025-02-14 14:50:54 +0700

    Cara Memanggil Object Di Dalam Array

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const orang = {
-  nama: "rama",
-  tanggal: 16,
-  alamat: "lampung",
-};
+const shoppingCart = [
+  { produt: "vue.js", price: 1.22, quantity: 3 },
+  { produt: "vue.js", price: 2.32, quantity: 1 },
+  { produt: "angular.js", price: 332, quantity: 2 },
+];
 
-orang["nama"] = "doni";
-
-console.log(orang.nama);
+console.log(shoppingCart[0].price);
