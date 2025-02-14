0b65596fe964c0d056209f56b0071295130d65f0
ramadoni16 <donicyberit@gmail.com>
2025-02-14 21:04:24 +0700

    mengenal perulangan

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const shoppingCart = [
-  { produt: "vue.js", price: 1.22, quantity: 3 },
-  { produt: "vue.js", price: 2.32, quantity: 1 },
-  { produt: "angular.js", price: 332, quantity: 2 },
-];
-
-console.log(shoppingCart[0].price);
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
