c37c302f34298db9def3ec28ea182d108b60b755
ramadoni16 <donicyberit@gmail.com>
2025-02-17 10:58:09 +0700

    Perulangan Menggunakan Perintah While

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const pilihan = "abcd";
+// MENGGUNAKAN WHILE
+// let num = 0;
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`${i}. Soal nomor ${i}:`);
-  for (let j = 0; j < pilihan.length; j++) {
-    console.log(`  ${pilihan[j]}. Pilihan Jawaban`);
-  }
+// while (num < 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// MENGGUNAKAN FOR
+// for (let num = 0; num < 10; num++) {
+//   console.log(num);
+// }
+
+const password = "Pass123";
+
+let guess = prompt("etnter the password");
+while (guess !== password) {
+  guess = prompt("enter the password");
 }
+alert("congrats! the password is correct");
