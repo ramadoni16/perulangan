91c5eca2b58e48bd8069e87476b10c13aa055ac2
ramadoni16 <donicyberit@gmail.com>
2025-02-21 14:10:57 +0700

    Cara Elegan Melakukan Perulangan Pada Array For Of

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-let maximum = parseInt(prompt("Masukkan nilai maksimal"));
-while (!maximum) {
-  maximum = parseInt(prompt("Masukkan nilai maksimal"));
-}
+const fruits = ["Banana", "orange", "Apple", "Manggo"];
 
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`Buah ${fruits[i]}`);
+// }
 
-let guess = parseInt(prompt("isi tebakan pertama kamu"));
-let attempts = 1;
+// for (let fruit of fruits) {
+//   console.log(`Buah ${fruit}`);
+// }
 
-while (parseInt(guess) !== targetNum) {
-  attempts++;
-  if (guess > targetNum) {
-    guess = prompt("Terlalu tinggi! tebak Lagi");
-  } else {
-    guess = prompt("Terlalu rendah! Tebak Lagi");
+const studentRow = [
+  ["olivia", "liam", "Emma", "Noah"],
+  ["Amelia", "oliver", "ava", "Elijah"],
+  ["Sophia", "Mateo", "Isabella", "Lucass"],
+];
+
+// for (let i = 0; i < studentRow.length; i++) {
+//   const row = studentRow[i];
+//   console.log(`Seat Row #${i + 1}`);
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(` ${row[j]}`);
+//   }
+// }
+
+for (let row of studentRow) {
+  for (let student of row) {
+    console.log(student);
   }
 }
-alert(`Selamat tebakan Anda Benar! Dengan percobaan ${attempts} kali`);
